ba9aed8698d55c1402c170c9b8fd4eaa4d71cf54
WadeFC <chimidixclusive@gmail.com>
2022-12-03 12:23:03 +0100

    upload changes

---
+++ simple console app/index.js
@@ -0,0 +1,93 @@
+// Take One
+
+// alert("Welcome to Wade and Sons Group od Stores!");
+
+// alert("See list of Products");
+//         alert(
+//             "Vintage T-shirt" + "\n" +
+//             " First Force One" + "\n" +
+//             " Plain Trouser" + "\n" +
+//             " Stock Jean" + "\n" +
+//             " Stock Jean" + "\n" +
+//             " Cat Sandals"
+//         );
+//     let Store = [  
+
+//             product = {name : "Vintage T-shirt", color :"White", size : "XL", },
+//             product = {name : "First Force One Cans", color :"White", size : "45",},
+//             product = {name : "Plain Trouser", color :"Black", size : "40", },
+//             product = {name : "Stock Jean", color :"Black", size : "41", },
+//             product = {name : "Stock Jean", color :"Blue", size : "41", },
+//             product = {name : "Cat Sandals ", color :"Black", size : "45", },
+//             ];
+//     let userChart = [];
+
+//     let data01 = prompt("Name of What you want to purchase");
+//     let data02 = prompt("Enter Your Size");
+//          let Order = Store.filter(product => product.name === data01)
+
+
+//     if (Order = true)
+//           {let entry = userChart.push(data01,data02)}
+//     else(alert(data01 + "is out of Stock"));
+
+//     if (userChart != 0){prompt("Input Card Details")};
+    
+//     alert("Payment Successful Proceed to Chart !")
+    
+    
+// Take Two
+        function ignite (){
+        alert("Welcome to Wade and Sons Group od Stores!");
+
+        alert("See list of Products");
+                alert(
+                    "Vintage T-shirt" + "\n" +
+                    " First Force One" + "\n" +
+                    " Plain Trouser" + "\n" +
+                    " Stock Jean" + "\n" +
+                    " Stock Jean" + "\n" +
+                    " Cat Sandals"
+                );
+        let Store = [  
+                product = {name : "Vintage T-shirt", color :"White", size : "XL", },
+                product = {name : "First Force One Cans", color :"White", size : "45",},
+                product = {name : "Plain Trouser", color :"Black", size : "40", },
+                product = {name : "Stock Jean", color :"Black", size : "41", },
+                product = {name : "Stock Jean", color :"Blue", size : "41", },
+                product = {name : "Cat Sandals ", color :"Black", size : "45", },
+                ];
+        let userChart = [];
+
+        let data01 = prompt("Name of What you want to purchase");
+        let data02 = prompt("Enter Your Size");
+            let Order = Store.filter(product => product.name === data01)
+
+        if (Order = true)
+             {let entry = userChart.push(data01,data02)}
+        else(Order !== true)
+            {alert(data01 + " is out of Stock")};
+
+        if (userChart !== 0){prompt("Input Card Details")};
+        
+        alert("Payment Successful Proceed to Chart !")};
+
+
+
+//    sample check 
+    // let search = prompt("enter order");
+
+    // let store = [  product = {name : "Vintage T-shirt", color :"White", size : "XL", },
+    //               product = {name : "First Force One Cans", color :"White", size : "45",},
+    //               product = {name : "Plain Trouser", color :"Black", size : "40", },
+    //               product = {name : "Stock Jean", color :"Black", size : "41", },
+    //               product = {name : "Stock Jean", color :"Blue", size : "41", },
+    //               product = {name : "Cat Sandals ", color :"Black", size : "45", },
+    //         ];
+    //         let order = store.filter(product => product.name === search)
+
+
+    //         if (order = true)
+    //     (alert(search + " is available"));
+
+
