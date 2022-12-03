8fa2c4f617919750c944ba4912916b5df527108b
WadeFC <chimidixclusive@gmail.com>
2022-12-03 13:40:28 +0100

    rectify

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 // Take One
 
-alert("Welcome to Wade and Sons Group od Stores!");
+// alert("Welcome to Wade and Sons Group od Stores!");
 
-alert("See list of Products");
-        alert(
-            "Vintage T-shirt" + "\n" +
-            " First Force One" + "\n" +
-            " Plain Trouser" + "\n" +
-            " Stock Jean" + "\n" +
-            " Stock Jean" + "\n" +
-            " Cat Sandals"
-        );
-    let Store = [  
+// alert("See list of Products");
+//         alert(
+//             "Vintage T-shirt" + "\n" +
+//             " First Force One" + "\n" +
+//             " Plain Trouser" + "\n" +
+//             " Stock Jean" + "\n" +
+//             " Stock Jean" + "\n" +
+//             " Cat Sandals"
+//         );
+//     let Store = [  
 
-            product = {name : "Vintage T-shirt", color :"White", size : "XL", },
-            product = {name : "First Force One Cans", color :"White", size : "45",},
-            product = {name : "Plain Trouser", color :"Black", size : "40", },
-            product = {name : "Stock Jean", color :"Black", size : "41", },
-            product = {name : "Stock Jean", color :"Blue", size : "41", },
-            product = {name : "Cat Sandals ", color :"Black", size : "45", },
-            ];
-    let userChart = [];
+//             product = {name : "Vintage T-shirt", color :"White", size : "XL", },
+//             product = {name : "First Force One Cans", color :"White", size : "45",},
+//             product = {name : "Plain Trouser", color :"Black", size : "40", },
+//             product = {name : "Stock Jean", color :"Black", size : "41", },
+//             product = {name : "Stock Jean", color :"Blue", size : "41", },
+//             product = {name : "Cat Sandals ", color :"Black", size : "45", },
+//             ];
+//     let userChart = [];
 
-    let data01 = prompt("Name of What you want to purchase");
-    let data02 = prompt("Enter Your Size");
-         let Order = Store.filter(product => product.name === data01)
+//     let data01 = prompt("Name of What you want to purchase");
+//     let data02 = prompt("Enter Your Size");
+//          let Order = Store.filter(product => product.name === data01)
 
 
-    if (Order = true)
-          {let entry = userChart.push(data01,data02)}
-    else(alert(data01 + "is out of Stock"));
+//     if (Order = true)
+//           {let entry = userChart.push(data01,data02)}
+//     else(alert(data01 + "is out of Stock"));
 
-    if (userChart != 0){prompt("Input Card Details")};
+//     if (userChart != 0){prompt("Input Card Details")};
     
-    alert("Payment Successful Proceed to Chart !")
+//     alert("Payment Successful Proceed to Chart !")
     
     
 // Take Two
